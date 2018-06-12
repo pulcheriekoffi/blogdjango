af9bcb5b53f5a892ca77a31faf1e8a477e4968ef
pulcherie <pulcherie125@gmail.com>
2018-06-12 14:06:55 +0000

    Debut de mon premier projet

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	"""docstring for PostForm"""
+	class Meta:
+		model = Post
+		fields = ('title', 'text',)
+		
---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
+]
+"""pour creation de vue views
+on vient dans urls.py d'abord on met le lien vers la vue et un name qui faire
+reference a une methode dans views
+creation d un templates post_detail.html
+"""
